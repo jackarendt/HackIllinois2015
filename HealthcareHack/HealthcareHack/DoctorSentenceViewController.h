cb8df64c5879c2afb20350ea294b90937e3b289d
jackarendt <jack.arendt93@gmail.com>
2015-02-28 00:09:20 -0600

    Created VC's and Full Storyboard updated

--- HealthcareHack/HealthcareHack/DoctorSentenceViewController.h
+++ HealthcareHack/HealthcareHack/DoctorSentenceViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  DoctorSentenceViewController.h
 //  HealthcareHack
 //
 //  Created by Jack Arendt on 2/27/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface DoctorSentenceViewController : UIViewController
 
 @end
-
---
+++ HealthcareHack/HealthcareHack/ProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.h
+//  HealthcareHack
+//
+//  Created by Jack Arendt on 2/27/15.
+//  Copyright (c) 2015 John Arendt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileViewController : UIViewController
+
+@end
