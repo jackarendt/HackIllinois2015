318f60fdc4f79f872c81219d329ea8ce0907fd9b
jackarendt <jack.arendt93@gmail.com>
2015-02-27 23:52:49 -0600

    Initial iOS Commit

---
+++ HealthcareHack/HealthcareHack/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HealthcareHack
+//
+//  Created by Jack Arendt on 2/27/15.
+//  Copyright (c) 2015 John Arendt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HealthcareHack/HealthcareHack/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HealthcareHack
+//
+//  Created by Jack Arendt on 2/27/15.
+//  Copyright (c) 2015 John Arendt. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
